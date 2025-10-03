1cb6a5a72769ebd70322fadbbf80d4226861dd79
YOUNGI-CHOI <chdbsrl@gmail.com>
2025-10-03 23:46:24 +0900

    first init

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+  res.end('Hello, World! YKCHOI 입니다.\n');
 });
 
 server.listen(3000, () => {
